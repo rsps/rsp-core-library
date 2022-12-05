31ac835e5d3398709d65dd6123103784c8c45180
steffen <steffen@steffen-Z690>
2022-12-05 16:16:27 +0100

    Add POSIX clock wrapper

---
+++ include/posix/Clock.h
@@ -0,0 +1,44 @@
+/*!
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * \copyright   Copyright (c) 2022 RSP Systems A/S. All rights reserved.
+ * \author      Steffen Brummer
+ */
+
+#ifndef INCLUDE_POSIX_CLOCK_H_
+#define INCLUDE_POSIX_CLOCK_H_
+
+#include <time.h>
+#include <utils/DateTime.h>
+
+namespace rsp::posix {
+
+/**
+ * \brief Wrapper for POSIX clock (gettime/settime) functions.
+ */
+class Clock
+{
+public:
+    enum class Types {
+        RealTime = CLOCK_REALTIME,
+        Monotonic = CLOCK_MONOTONIC, // Monotonic system-wide clock.
+        HighResolution = CLOCK_PROCESS_CPUTIME_ID, // High-resolution timer from the CPU.
+        ThreadTime = CLOCK_THREAD_CPUTIME_ID, // Thread-specific CPU-time clock.
+        MonotonicRaw = CLOCK_MONOTONIC_RAW, // Monotonic system-wide clock, not adjusted for frequency scaling.
+        RealTimeCoarse = CLOCK_REALTIME_COARSE, // Identifier for system-wide realtime clock, updated only on ticks.
+        MonotonicCoarse = CLOCK_MONOTONIC_COARSE, // Monotonic system-wide clock, updated only on ticks.
+        BootTime = CLOCK_BOOTTIME, // Monotonic system-wide clock that includes time spent in suspension.
+        RealTimeAlarm = CLOCK_REALTIME_ALARM, // Like CLOCK_REALTIME but also wakes suspended system.
+        BootTimeAlarm = CLOCK_BOOTTIME_ALARM, // Like CLOCK_BOOTTIME but also wakes suspended system.
+        TAI = CLOCK_TAI // Like CLOCK_REALTIME but in International Atomic Time.
+    };
+
+    static void SetTime(Types aClockType, const rsp::utils::DateTime &arDT);
+    static rsp::utils::DateTime GetTime(Types aClockType);
+};
+
+} /* namespace rsp::posix */
+
+#endif /* INCLUDE_POSIX_CLOCK_H_ */
---
+++ src/posix/Clock.cpp
@@ -0,0 +1,36 @@
+/*!
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * \copyright   Copyright (c) 2022 RSP Systems A/S. All rights reserved.
+ * \author      Steffen Brummer
+ */
+
+#include <posix/Clock.h>
+#include <utils/CoreException.h>
+
+using namespace rsp::utils;
+
+namespace rsp::posix {
+
+void Clock::SetTime(Types aClockType, const rsp::utils::DateTime &arDT)
+{
+    timespec ts = arDT.GetTimeSpec();
+    int res = clock_settime(static_cast<int>(aClockType), &ts);
+    if (res < 0) {
+        THROW_SYSTEM("Failed to set system clock");
+    }
+}
+
+DateTime Clock::GetTime(Types aClockType)
+{
+    timespec ts;
+    int res = clock_gettime(static_cast<int>(aClockType), &ts);
+    if (res < 0) {
+        THROW_SYSTEM("Failed to get system clock");
+    }
+    return DateTime(ts);
+}
+
+} /* namespace rsp::posix */
