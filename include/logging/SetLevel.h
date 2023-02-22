a4588d34a184d56744d4ba277489e3db39ea476d
kernelguy <steffen@rspsystems.com>
2023-02-22 13:00:19 +0100

    Add SetLevel manipulator used to set acceptance leven on log streams

---
+++ include/logging/SetLevel.h
@@ -0,0 +1,56 @@
+/*!
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * \copyright   Copyright 2023 RSP Systems A/S. All rights reserved.
+ * \license     Mozilla Public License 2.0
+ * \author      Steffen Brummer
+ */
+
+#ifndef INCLUDE_LOGGING_SETLEVEL_H_
+#define INCLUDE_LOGGING_SETLEVEL_H_
+
+#include <ostream>
+#include "LogTypes.h"
+
+namespace rsp::logging {
+
+/**
+ * \class SetLevel
+ * \brief Stream manipulator to set the log accept level of log streams
+ *
+ */
+class SetLevel
+{
+public:
+    /**
+     * \brief Construct object with LogLevel value
+     *
+     * \param aLevel
+     */
+    SetLevel(LogLevel aLevel) : mValue(aLevel) {}
+
+    /**
+     * \brief Callable operator used by streaming operators
+     *
+     * \param o
+     * \return
+     */
+    std::ostream& operator()(std::ostream& o) const;
+
+    LogLevel mValue;
+};
+
+/**
+ * \brief Streaming operator for SetLevel object
+ *
+ * \param o
+ * \param aLevel
+ * \return
+ */
+std::ostream& operator<<(std::ostream &o, SetLevel aLevel);
+
+} /* namespace rsp::logging */
+
+#endif /* INCLUDE_LOGGING_SETLEVEL_H_ */
