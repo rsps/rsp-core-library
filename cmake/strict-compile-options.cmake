fd9ed442e84127269b979844b5d8471b0ed8f8c1
Steffen Brummer <steffen@kernelguy.dk>
2023-02-27 13:49:20 +0100

    Add cmake folder with usefull scripts

---
+++ cmake/strict-compile-options.cmake
@@ -0,0 +1,28 @@
+add_compile_options(
+    -pedantic
+    -Wall
+    -Wextra
+    -Wold-style-cast
+    -Wcast-align
+    -Wcast-qual
+    -Wctor-dtor-privacy
+    -Wdisabled-optimization
+    -Wformat=2
+    -Winit-self
+    -Wlogical-op
+    -Wmissing-declarations
+    -Wmissing-include-dirs
+    -Wnoexcept
+    -Woverloaded-virtual
+    -Wredundant-decls
+    -Wsign-conversion
+    -Wsign-promo
+    -Wstrict-null-sentinel
+    -Wstrict-overflow=3
+#    -Wswitch-default
+    -Wundef
+    -Wno-unused
+    -Weffc++
+    -Werror
+#     -v -dD
+)
