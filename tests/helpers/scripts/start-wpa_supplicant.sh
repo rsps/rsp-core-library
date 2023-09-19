810c1b32c0f9cec7a8462c6ccb78cc32e5c31eb8
kernelguy <steffen@rspsystems.com>
2023-09-19 10:46:47 +0200

    Fixed start-wpa_supplicant.sh to lookup correct interface

--- tests/helpers/scripts/start-wpa_supplicant.sh
+++ tests/helpers/scripts/start-wpa_supplicant.sh
@@ -8,7 +8,9 @@ if ! is_user_root; then
     SUDO='sudo'
 fi
 
+INTERFACE=`nmcli --get-values GENERAL.DEVICE,GENERAL.TYPE device show | awk '/^wifi$/{print dev; next};{dev=$0};'`
+
 $SUDO systemctl mask wpa_supplicant.service
 $SUDO systemctl stop wpa_supplicant.service
 
-$SUDO wpa_supplicant -B -i wlx200db0144e41 -c /etc/wpa_supplicant/wpa_supplicant.conf
+$SUDO wpa_supplicant -B -i $INTERFACE -c /etc/wpa_supplicant/wpa_supplicant.conf
