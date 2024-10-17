600fd19c16d1f49b3ab82ce4075303721a60c3f7
Steffen Brummer <steffen@kernelguy.dk>
2024-10-17 13:13:04 +0200

    Add useful AbstractIterator for containers with abstract derivatives

---
+++ include/utils/AbstractIterator.h
@@ -0,0 +1,81 @@
+/**
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at https://mozilla.org/MPL/2.0/.
+*
+* \copyright   Copyright 2024 RSP Systems A/S. All rights reserved.
+* \license     Mozilla Public License 2.0
+* \author      steffen
+*/
+#ifndef RSP_CORE_LIB_INCLUDE_UTILS_ABSTRACT_ITERATOR_H
+#define RSP_CORE_LIB_INCLUDE_UTILS_ABSTRACT_ITERATOR_H
+
+#include <cstdint>
+#include <iterator>
+
+namespace rsp::utils {
+
+/**
+ * \brief Template class to define a forward iterator that can be used to traverse abstract types.
+ * \tparam T Abstract type to iterate over
+ *
+ * For a full contiguous iterator \see https://stackoverflow.com/a/75061822
+ */
+template<class T>
+struct AbstractIterator
+{
+    using iterator_category = std::forward_iterator_tag;
+    using difference_type   = std::ptrdiff_t;
+    using value_type        = T;
+    using pointer           = value_type *;
+    using reference         = value_type &;
+    using size_type         = size_t;
+
+    constexpr auto cAdd(pointer aPtr, difference_type aOffset) {
+        if constexpr (std::is_const_v<value_type>) {
+            return reinterpret_cast<pointer>(reinterpret_cast<const uint8_t *>(aPtr) + (aOffset * difference_type(mElementSize)));
+        }
+        else {
+            return reinterpret_cast<pointer>(reinterpret_cast<uint8_t *>(aPtr) + (aOffset * difference_type(mElementSize)));
+        }
+    }
+
+private:
+    pointer mPtr = nullptr, mStart = nullptr, mSentinel = nullptr;
+    size_type mElementSize = 0;
+    static_assert(std::sentinel_for<decltype(mSentinel), decltype(mPtr)>);
+
+public:
+    AbstractIterator() { throw std::runtime_error("Not implemented"); }
+    AbstractIterator(pointer aStart, pointer aSentinel, size_type aDerivedElementSize)
+        : AbstractIterator(aStart, aStart, aSentinel, aDerivedElementSize) {}
+    AbstractIterator(pointer aPtr, pointer aStart, pointer aSentinel, size_type aDerivedElementSize)
+        : mPtr(aPtr), mStart(aStart), mSentinel(aSentinel), mElementSize(aDerivedElementSize) {}
+
+    reference operator*() const { return *mPtr; }
+    pointer operator->() { return mPtr; }
+
+    AbstractIterator operator+(const difference_type offset) const { return { cAdd(mPtr, offset), mStart, mSentinel, mElementSize }; }
+    friend AbstractIterator operator+(const difference_type offset, const AbstractIterator &other) {
+        return other + offset;
+    }
+
+    AbstractIterator& operator+=(const difference_type offset) { mPtr = cAdd(mPtr, offset); return *this; }
+
+    // Prefix increment
+    AbstractIterator& operator++() { mPtr = cAdd(mPtr, 1);; return *this; }
+
+    // Postfix increment
+    AbstractIterator operator++(int) { auto tmp = *this; ++(*this); return tmp; }
+
+    friend bool operator==(const AbstractIterator& a, const AbstractIterator& b) { return a.mPtr == b.mPtr; };
+    friend bool operator!=(const AbstractIterator& a, const AbstractIterator& b) { return a.mPtr != b.mPtr; };
+    auto operator<=>(const AbstractIterator &) const = default; // three-way comparison C++20
+
+    auto begin() const { return AbstractIterator(mStart, mSentinel, mElementSize); }
+    auto end() const { return AbstractIterator(mSentinel, mStart, mSentinel, mElementSize); }
+};
+
+} // namespace rsp::utils
+
+#endif //RSP_CORE_LIB_INCLUDE_UTILS_ABSTRACT_ITERATOR_H
---
+++ tests/unit/utils/test-AbstractIterator.cpp
@@ -0,0 +1,142 @@
+/**
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at https://mozilla.org/MPL/2.0/.
+*
+* \copyright   Copyright 2024 RSP Systems A/S. All rights reserved.
+* \license     Mozilla Public License 2.0
+* \author      steffen
+*/
+
+#include "doctest.h"
+#include <algorithm>
+#include <utils/AbstractIterator.h>
+#include <vector>
+
+using namespace rsp::utils;
+
+struct IElement
+{
+    virtual ~IElement() = default;
+    [[nodiscard]] virtual int GetValue() const = 0;
+    virtual void SetValue(int aValue) = 0;
+};
+
+struct IContainer
+{
+    using Iterator = AbstractIterator<IElement>;
+    using ConstIterator = AbstractIterator<const IElement>;
+    static_assert(std::forward_iterator<Iterator>);
+    static_assert(std::forward_iterator<ConstIterator>);
+
+    virtual ~IContainer() = default;
+    virtual Iterator begin() = 0;
+    virtual Iterator end() = 0;
+    virtual ConstIterator cbegin() = 0;
+    virtual ConstIterator cend() = 0;
+    virtual size_t size() = 0;
+};
+
+class Element : public IElement
+{
+public:
+    explicit Element(int aValue) noexcept : mValue(aValue) {}
+
+    [[nodiscard]] int GetValue() const override
+    {
+        return mValue;
+    }
+    void SetValue(int aValue) override
+    {
+        mValue = aValue;
+    }
+protected:
+    int mValue;
+};
+
+class Container : public IContainer
+{
+public:
+    Container()
+    {
+        for (int i = 0; i < 5 ; i++) {
+            mVector.emplace_back(i);
+        }
+    }
+
+    Iterator begin() override
+    {
+        return { &mVector.front(), &mVector.back() + 1, sizeof(Element) };
+    }
+
+    Iterator end() override
+    {
+        return Iterator(&mVector.front(), &mVector.back() + 1, sizeof(Element)).end();
+    }
+
+    ConstIterator cbegin() override
+    {
+        return { &mVector.front(), &mVector.back() + 1, sizeof(Element) };
+    }
+
+    ConstIterator cend() override
+    {
+        return ConstIterator(&mVector.front(), &mVector.back() + 1, sizeof(Element)).end();
+    }
+
+    size_t size() override
+    {
+        return mVector.size();
+    }
+
+protected:
+    std::vector<Element> mVector{};
+};
+
+
+TEST_CASE("AbstractIterator")
+{
+    CHECK_NOTHROW(Container dummy);
+
+    Container container;
+
+    SUBCASE("for-loop") {
+        int expected = 0;
+        size_t count = 0;
+
+        for (auto &e: container) {
+            CHECK_EQ(e.GetValue(), expected++);
+            count++;
+        }
+        CHECK_EQ(count, container.size());
+    }
+
+    SUBCASE("find") {
+        auto cit = std::find_if(container.cbegin(), container.cend(), [](const IElement &e) {
+            return e.GetValue() == 4;
+        });
+        CHECK_NE(cit, container.cend());
+//        cit->SetValue(5); // This does not compile
+        cit++;
+        CHECK_EQ(cit, container.cend());
+
+        auto it = std::find_if(container.begin(), container.end(), [](const IElement &e) {
+            return e.GetValue() == 4;
+        });
+        CHECK_NE(it, container.end());
+        it->SetValue(5);
+        it++;
+        CHECK_EQ(it, container.end());
+
+        cit = std::find_if(container.cbegin(), container.cend(), [](const IElement &e) {
+            return e.GetValue() == 4;
+        });
+        CHECK_EQ(cit, container.cend());
+
+        cit = std::find_if(container.cbegin(), container.cend(), [](const IElement &e) {
+            return e.GetValue() == 5;
+        });
+        CHECK_NE(cit, container.cend());
+    }
+
+}
