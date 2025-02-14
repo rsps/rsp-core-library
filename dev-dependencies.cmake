878a9af634b6966ee0ce90da7fce15de6663a239
alin <alin@rspsystems.com>
2025-02-14 13:10:28 +0100

    Add Core Lib dev-dependencies

--- dev-dependencies.cmake
+++ dev-dependencies.cmake
@@ -10,10 +10,14 @@ include("dependencies.cmake")
 macro(install_dev_dependencies)
     message(NOTICE "${COLOR_WHITE}Installing Development Dependencies for${RESTORE} ${PROJECT_NAME}")
 
-    # Avoid building tests for dependencies...
-    set(BUILD_TESTING off)
+    # -------------------------------------------------------------------------------------------------------------- #
 
-    # Add dev-dependencies here...
-    message(VERBOSE "    N/A")
+    include("external/doctest")
+    include("external/lighttpd")
+
+    # -------------------------------------------------------------------------------------------------------------- #
+
+    # ...etc
+
 endmacro()
-safeguard_properties("install_dev_dependencies" "BUILD_TESTING")
+safeguard_properties("install_dev_dependencies" "")
