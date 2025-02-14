95b7f561080c6d5b30ccd128004a0516540b7521
alin <alin@rspsystems.com>
2025-02-14 11:36:21 +0100

    Add WPA Supplicant dependency script

---
+++ cmake/external/wpa-supplicant.cmake
@@ -0,0 +1,33 @@
+# -------------------------------------------------------------------------------------------------------------- #
+# WPA Supplicant
+# @see https://trac.netlabs.org/ports/wiki/wpa_supplicant
+#
+# @see https://packages.debian.org/sid/libwpa-client-dev
+# -------------------------------------------------------------------------------------------------------------- #
+
+include_guard(GLOBAL)
+
+# Debug
+message(VERBOSE "Installing WPA Supplicant")
+
+# NOTE: This is a runtime dependency that SHOULD be installed on the system
+#       sudo apt install libwpa-client-dev
+
+# find_package(WPA_SUPPLICANT REQUIRED) # NOT going to work without a custom `FindWPASupplicant` module
+
+find_library(wpa_supplicant NAMES "wpa_client" REQUIRED)
+
+# Debug
+if (DEFINED "wpa_supplicant-NOTFOUND")
+    message(STATUS "WPA Supplicant (${COLOR_RED}${TEXT_ITALIC}not found${RESTORE})")
+else ()
+    message(STATUS "Found WPA Supplicant (${COLOR_WHITE}${TEXT_ITALIC}${wpa_supplicant}${RESTORE})")
+endif ()
+
+# Include & link...
+target_link_libraries(${PROJECT_NAME} wpa_client)
+
+# -------------------------------------------------------------------------------------------------------------- #
+
+# Configure build options
+# N/A
