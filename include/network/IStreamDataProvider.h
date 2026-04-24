fe9acb11cb699c57d78fd5d1793eae0f3a8d333b
Kim Lindberg Schwaner <kim@rspsystems.com>
2026-04-24 12:40:42 +0200

    Take ref a non-const to avoid const_cast<>'ing

--- include/network/IStreamDataProvider.h
+++ include/network/IStreamDataProvider.h
@@ -90,9 +90,9 @@ public:
     }
 };
 
-inline std::ostream& operator<<(std::ostream& o, const IStreamDataProvider &arStream)
+inline std::ostream& operator<<(std::ostream& o, IStreamDataProvider &arStream)
 {
-    const_cast<IStreamDataProvider&>(arStream).Rewind();
+    arStream.Rewind();
     return arStream.PrintContent(o);
 }
 
