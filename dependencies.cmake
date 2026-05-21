9a927012087481f451707dbf1c7d95520b4920a1
Kim Lindberg Schwaner <kim@rspsystems.com>
2026-05-21 10:10:40 +0200

    Bumb magic_enum version to v0.9.8
    See https://github.com/Neargye/magic_enum/releases/tag/v0.9.8

--- dependencies.cmake
+++ dependencies.cmake
@@ -48,7 +48,7 @@ function(rsp_core_add_dependencies ATARGET)
     FetchContent_Declare(
         magic_enum
         GIT_REPOSITORY https://github.com/Neargye/magic_enum
-        GIT_TAG v0.9.7
+        GIT_TAG v0.9.8
         GIT_SHALLOW TRUE
     )
     FetchContent_MakeAvailable(magic_enum)
