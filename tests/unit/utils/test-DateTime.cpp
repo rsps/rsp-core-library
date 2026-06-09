ba520e51c34cdcea56791faae2e2a5086ad73f36
Kim Lindberg Schwaner <kim@rspsystems.com>
2026-06-09 15:50:33 +0200

    Replace call to asctime() with std::put_time()
    `asctime()` is potentially unsafe because it writes into a static,
    fixed-size internal buffer which can overflow.

--- tests/unit/utils/test-DateTime.cpp
+++ tests/unit/utils/test-DateTime.cpp
@@ -162,7 +162,7 @@ TEST_CASE("DateTime")
 
             DateTime dt(tm);
             std::stringstream ss;
-            ss << std::asctime(&tm);
+            ss << std::put_time(&tm, "%c");
             DMESG("DateTime(" << ss.str() << ") = " << dt)
             DMESG("DateTime(" << cISO8601UTC << ") = " << dt.ToISO8601UTC())
             CHECK_EQ(dt.ToISO8601UTC(), cISO8601UTC);
