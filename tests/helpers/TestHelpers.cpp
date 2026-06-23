01b62a531c600aeedb05aebd593d481bad2a60ef
Kim Lindberg Schwaner <kim@rspsystems.com>
2026-06-23 14:44:10 +0200

    Search only PATH for lighttpd executable
    Instead of hard-coded paths we simply search the system PATH.

--- tests/helpers/TestHelpers.cpp
+++ tests/helpers/TestHelpers.cpp
@@ -81,28 +81,9 @@ bool TestHelpers::ValidateJsonFile(const std::string& arJsonFile)
 
 int TestHelpers::StartWebServer()
 {
-    // Find lighttpd executable
-    static constexpr std::string_view PATHS[] = {
-            "/usr/sbin/lighttpd", "/usr/bin/lighttpd",
-            "/usr/local/sbin/lighttpd", "/usr/local/bin/lighttpd"};
-
-    std::string_view exe = [] {
-        for (const auto& p : PATHS) {
-            if (std::filesystem::exists(p)) {
-                return p;
-            }
-        }
-        return std::string_view{};
-    }();
-
-    // Fall back to searching PATH
-    if (exe.empty()) {
-        exe = "lighttpd";
-    }
-
     [[maybe_unused]] int rc = std::system("killall lighttpd -q"); // Make sure it is not running
     std::string cwd = std::filesystem::current_path();
-    std::string command = std::format("LIGHTTPD_BASEDIR='{}/webserver' {} -f '{}/webserver/lighttpd.conf'", cwd, exe, cwd);
+    std::string command = std::format("LIGHTTPD_BASEDIR='{}/webserver' lighttpd -f '{}/webserver/lighttpd.conf'", cwd, cwd);
     return std::system(command.c_str());
 }
 
